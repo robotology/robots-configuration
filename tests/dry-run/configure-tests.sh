2449fe8b68e47ae5ca2d7fe25dcc0b48f6def217
Ugo Pattacini <ugo.pattacini@iit.it>
2024-12-30 17:04:09 +0000

    fixed ci configuration

--- tests/dry-run/configure-tests.sh
+++ tests/dry-run/configure-tests.sh
@@ -11,3 +11,4 @@ cmake -DCMAKE_PREFIX_PATH=${DEPENDENCIES_DIR}/install \
       -DCMAKE_INSTALL_PREFIX=${DEPENDENCIES_DIR}/install \
       -DBUILD_TESTING:BOOL=ON \
       -DINSTALL_ALL_ROBOTS:BOOL=ON ../../../
+cmake --build . --config Release --target install
